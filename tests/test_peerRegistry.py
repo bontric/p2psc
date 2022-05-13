b6aead0c19c19e890abb5775888e03f7e3df5ffd
Benedikt Wieder <bontric@riseup.net>
2022-05-13 12:13:49 +0200

    fix peer registry test

--- tests/test_peerRegistry.py
+++ tests/test_peerRegistry.py
@@ -1,11 +1,10 @@
-
-
+import pytest
 from p2psc.peerRegistry import PeerRegistry
 from p2psc.peerInfo import PeerInfo, PeerType
 
 
 def test_get_by_type():
-    reg = PeerRegistry()
+    reg = PeerRegistry("name")
     pi_client = PeerInfo(("127.0.0.1", 1), type=PeerType.client)
     pi_node = PeerInfo(("127.0.0.1", 2), type=PeerType.node)
     reg.add_peer(pi_client)
@@ -21,23 +20,26 @@ def test_get_by_type():
 
 
 def test_get_local():
-    reg = PeerRegistry()
-    groups = ["A", "B", "ALL"]
+    reg = PeerRegistry("name")
+    groups = [ "A", "B", "ALL", "name"]
     paths = ["a", "b"]
     not_groups = ["C"]
     not_paths = ["c"]
     pi_client = PeerInfo(
-        ("127.0.0.1", 1), groups=groups[0], paths=paths[0], type=PeerType.client)
+        ("127.0.0.1", 1), groups=[groups[0]], paths=[paths[0]], type=PeerType.client)
     pi_client2 = PeerInfo(
-        ("127.0.0.1", 2), groups=groups[1], paths=paths[1], type=PeerType.client)
+        ("127.0.0.1", 2), groups=[groups[1]], paths=[paths[1]], type=PeerType.client)
     pi_node = PeerInfo(
-        ("127.0.0.1", 3), groups=not_groups[0], paths=not_paths[0], type=PeerType.node)
+        ("127.0.0.1", 3), groups=[not_groups[0]], paths=[not_paths[0]], type=PeerType.node)
     reg.add_peer(pi_client)
     reg.add_peer(pi_client2)
     reg.add_peer(pi_node)
 
     lgroups = reg.get_local_groups()
-    assert lgroups == groups
+    for g in groups:
+        assert g in lgroups
 
     lpaths = reg.get_local_paths()
+    for p in paths:
+        assert p in lpaths
     assert lpaths == paths
