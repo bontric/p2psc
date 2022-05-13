fe65f1262a7ba1285434801c912fa23907f1f89a
Benedikt Wieder <bontric@riseup.net>
2022-05-13 12:16:02 +0200

    Add some doc to tests

--- tests/test_peerRegistry.py
+++ tests/test_peerRegistry.py
@@ -35,10 +35,12 @@ def test_get_local():
     reg.add_peer(pi_client2)
     reg.add_peer(pi_node)
 
+    # Check if groups match expected
     lgroups = reg.get_local_groups()
     for g in groups:
         assert g in lgroups
 
+    # Check if paths match expected
     lpaths = reg.get_local_paths()
     for p in paths:
         assert p in lpaths
@@ -51,15 +53,17 @@ def test_cleanup():
     reg.add_peer(pi_client)
     reg.add_peer(pi_node)
 
+    # Check node is NOT removed if it is not expired
+    assert pi_node.is_expired() == False
     reg.cleanup()
-
     assert reg.get_peer(pi_client.addr) == pi_client
     assert reg.get_peer(pi_node.addr) == pi_node
 
+
+    # Check whether node is removed after expiry
     pi_node.last_update_t = 0
-
+    assert pi_node.is_expired() == True
     reg.cleanup()
-
     assert reg.get_peer(pi_client.addr) == pi_client
     with pytest.raises(LookupError):
         reg.get_peer(pi_node.addr)
